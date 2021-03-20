767fc5a99ab0465561421fbf6032b9608734d341
ropr123 <m.rohit.prabhu@gmail.com>
2021-03-20 20:16:25 +0530

    made changes for one motor driver use case

--- motor_code/rover.py
+++ motor_code/rover.py
@@ -1,5 +1,5 @@
 import socket 
-from wheel import Wheels
+rightom wheel import Wheels
 
 #socket setup
 s=socket.socket()
@@ -7,66 +7,47 @@ ip="192.168.0.173"
 port=9999
 s.connect((ip,port))
 
+#speed declaration
+speed=50
 
 #wheel setup
-fl=Wheels(13,15,33,0)
-fr=Wheels(16,18,12,0)
-bl=Wheels(29,31,35,0)
-br=Wheels(22,36,32,0)
+left=Wheels(15,13,33,0)
+right=Wheels(16,18,12,0)
 
 while True:
 	d=str(s.recv(1024),"utf-8")
 	print(d)
 	if d=='w':
-		fl.control(1,0,speed)
-		fr.control(1,0,speed)
-		bl.control(1,0,speed)
-		br.control(1,0,speed) 
+		left.control(1,0,speed)
+		right.control(1,0,speed)
 	elif d=='s':
-		fl.control(0,1,speed)
-		fr.control(0,1,speed)
-		bl.control(0,1,speed)
-		br.control(0,1,speed) 
+		left.control(0,1,speed)
+		right.control(0,1,speed) 
 	elif d=='w+a' or d=='a+w':
-		fl.control(1,0,speed/2)
-		fr.control(1,0,speed)
-		bl.control(1,0,speed/2)
-		br.control(1,0,speed) 
+		left.control(1,0,speed/2)
+		right.control(1,0,speed)
 	elif d=='w+d' or d=='d+w':
-		fl.control(1,0,speed)
-		fr.control(0,0,speed/2)
-		bl.control(1,0,speed)
-		br.control(0,0,speed/2) 
+		left.control(1,0,speed)
+		right.control(1,0,speed/2)
 	elif d=='s+a' or d=='a+s':
-		fl.control(0,0,speed/2)
-		fr.control(0,1,speed)
-		bl.control(0,0,speed/2)
-		br.control(0,1,speed) 
+		left.control(0,1,speed/2)
+		right.control(0,1,speed)
 	elif d=='s+d' or d=='d+s':
-		fl.control(0,1,speed)
-		fr.control(0,0,speed/2)
-		bl.control(0,1,speed)
-		br.control(0,0,speed/2) 
+		left.control(0,1,speed)
+		right.control(0,1,speed/2)
 	elif d=='a':
-		fl.control(0,1,speed)
-		fr.control(1,0,speed)
-		bl.control(0,1,speed)
-		br.control(1,0,speed) 
+		left.control(0,1,speed)
+		right.control(1,0,speed)
 	elif d=='d':
-		fl.control(1,0,speed)
-		fr.control(0,1,speed)
-		bl.control(1,0,speed)
-		br.control(0,1,speed) 		
+		left.control(1,0,speed)
+		right.control(0,1,speed)	
 	elif d=='e':
 		break
 	else:
-		fl.control(0,0,0)
-		fr.control(0,0,0)
-		bl.control(0,0,0)
-		br.control(0,0,0) 
+		left.control(0,0,0)
+		right.control(0,0,0)
 
-fl.hault()
-fr.hault()
-br.hault()
-bl.hault()
+left.hault()
+right.hault()
+
 s.close()
