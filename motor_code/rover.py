87130eddcc21568dfd3cbebc49ee4c572f8ed066
Rahul <rahulreddy19981998@gmail.com>
2021-03-21 00:05:54 +0530

    First working code
    At this point the rover was moving!

--- motor_code/rover.py
+++ motor_code/rover.py
@@ -1,18 +1,18 @@
 import socket 
-rightom wheel import Wheels
+from wheel import Wheels
 
 #socket setup
 s=socket.socket()
-ip="192.168.0.173"
+ip="192.168.0.29"
 port=9999
 s.connect((ip,port))
 
 #speed declaration
-speed=50
+speed=25
 
 #wheel setup
-left=Wheels(15,13,33,0)
-right=Wheels(16,18,12,0)
+left=Wheels(13,15,33,0)
+right=Wheels(18,16,12,0)
 
 while True:
 	d=str(s.recv(1024),"utf-8")
@@ -25,15 +25,15 @@ while True:
 		right.control(0,1,speed) 
 	elif d=='w+a' or d=='a+w':
 		left.control(1,0,speed/2)
-		right.control(1,0,speed)
+		right.control(1,0,speed*2)
 	elif d=='w+d' or d=='d+w':
-		left.control(1,0,speed)
+		left.control(1,0,speed*2)
 		right.control(1,0,speed/2)
 	elif d=='s+a' or d=='a+s':
 		left.control(0,1,speed/2)
-		right.control(0,1,speed)
+		right.control(0,1,speed*2)
 	elif d=='s+d' or d=='d+s':
-		left.control(0,1,speed)
+		left.control(0,1,speed*2)
 		right.control(0,1,speed/2)
 	elif d=='a':
 		left.control(0,1,speed)
