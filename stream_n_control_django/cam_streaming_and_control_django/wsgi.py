6924c2fcd5910232dcf1e0b239092037013f0911
Mayank saini <mayank98108@gmail.com>
2021-03-31 12:50:35 +0530

    Changed streaming server from flask to django
    also made changes in the frame format for jpeg because of different httpresponse object in django library, i will add control function ASAP

---
+++ stream_n_control_django/cam_streaming_and_control_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cam_streaming_and_control_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cam_streaming_and_control_django.settings')
+
+application = get_wsgi_application()
