4d03ba0c2b51be33253588b123853fce82acc4e9
skynet-05 <rahulreddy19981998@gmail.com>
2021-04-06 21:23:59 +0530

    added cam stream to server code

--- motor_code/Server_code.py
+++ motor_code/Server_code.py
@@ -1,13 +1,17 @@
 import keyboard
 import time
 import socket
+import webbrowser
+
 s=socket.socket()
-host="192.168.0.100"
+host="192.168.0.10"
 port=9999
 s.bind((host,port))
 s.listen(5)
 c,a=s.accept()
 print(a[0])
+URL="http://"+a[0]+":5000"
+webbrowser.open(URL, new=2)
 mes='x'
 while True:
     if (keyboard.is_pressed('a') or keyboard.is_pressed('w') or keyboard.is_pressed('s') or keyboard.is_pressed('d') or keyboard.is_pressed('a+w') or keyboard.is_pressed('w+d') or keyboard.is_pressed('w+a') or keyboard.is_pressed('d+w') or keyboard.is_pressed('s+a') or keyboard.is_pressed('a+s') or keyboard.is_pressed('s+d') or keyboard.is_pressed('d+s') or keyboard.is_pressed('e')):
