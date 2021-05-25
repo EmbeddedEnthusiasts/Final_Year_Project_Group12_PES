ea98a560f27b3cb5ee3f85a6c13ae573a5c8d366
skynet-05 <rahulreddy19981998@gmail.com>
2021-05-25 20:07:52 +0530

    Working and tested Ultrasonic code

--- ultrasonic_test/senseaquisition.py
+++ ultrasonic_test/senseaquisition.py
@@ -1,26 +1,20 @@
-from flask import Flask
-import json
-from random import seed, randint
+from flask import *
 import hcsr
 
 left=hcsr.Ultrasonic(16,18)
-
-seed(1)
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def get():
-    data={
-        "left": left.getDistance(),
-        # "right":randint(0,10),
-        # "back":randint(0,10),
-    }
-    data_json=json.dumps(data)
-    return data_json
+    return jsonify(
+        l = left.getDistance()
+    )
 
+@app.route('/home')
+def index():
+    return render_template('index.html')
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="192.168.0.20", debug=True)
 
