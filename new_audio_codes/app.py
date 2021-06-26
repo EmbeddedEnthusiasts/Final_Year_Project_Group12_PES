a438b950a2c209576bc360a19e8976248ae2c608
skynet-05 <rahulreddy19981998@gmail.com>
2021-06-26 13:38:03 +0530

    new audio code

---
+++ new_audio_codes/app.py
@@ -0,0 +1,42 @@
+from flask import Flask,send_from_directory
+import pyaudio
+import wave
+
+
+app=Flask(__name__)
+
+@app.route('/')
+
+filename = "recorded.wav"
+chunk = 1024
+FORMAT = pyaudio.paInt16
+channels = 1
+sample_rate = 44100
+record_seconds=0.3
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=channels,
+                rate=sample_rate,
+                input=True,
+                output=True,
+                frames_per_buffer=chunk)
+
+def  liveaudio():
+    frames = []
+    for i in range(int(44100 / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(filename, "wb")
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(sample_rate)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+    return send_from_directory("/home/pi/Desktop/Final_Year_Project_Group12_PES/new_audio_codes", "recorded.wav")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
